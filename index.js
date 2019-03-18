9cb40aa2e98d45b597498eca042c38ce1d9cb4ca
BuildTools <unconfigured@null.spigotmc.org>
2019-03-18 15:28:02 -0400

    added admin page alpha 0.1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const mongoClient = require('mongodb').MongoClient;
 
-var mongoUrl = "mongodb://192.168.1.193:27017/";
+var mongoUrl = "mongodb://localhost:27017/";
 var bodyParser = require('body-parser');
 
 // Use req.query to read values!!
@@ -19,17 +19,19 @@ app.get('/profile', (req, res) => {
 	res.sendFile(__dirname + "/pages/profile.html")});
 app.get('/edit_profile', (req, res) => {
 	res.sendFile(__dirname + "/pages/edit_profile.html")});
+app.get('/admin.html', (req, res) => {
+	res.sendFile(__dirname + "/pages/admin.html")});
 app.get('/testScript.js', (req, res) => {
 	res.sendFile(__dirname + "/pages/testScript.js")});
-app.get('/index.js', (req, res) => {
-	res.sendFile(__dirname + "/index.js")});
-app.get('/api/test', (req, res) => {
+app.get('/database.js', (req, res) => {
+	res.sendFile(__dirname + "/pages/database.js")});
+app.get('/api/getUser', (req, res) => {
 	mongoClient.connect(mongoUrl, function(err, db) {
 		if (err) throw err;
-		var dbo = db.db("apps4rvaTest");
-		dbo.collection("users").findOne({user: req.query.username}, function(err, result) {
+		var dbo = db.db("apps4rva");
+		dbo.collection("users").findOne({username: req.query.username}, function(err, result) {
 			if (err) throw err;
-			res.end(JSON.stringify(result))
+			res.end(JSON.stringify(result));
 			db.close();
 			});
 		}
